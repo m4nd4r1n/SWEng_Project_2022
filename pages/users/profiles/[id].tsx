e59b6023ea6fcc89d004c8a6b34a031947709d36
m4nd4r1n <kara.mandarin99@gmail.com>
2022-06-04 15:12:52 +0900

    신고 버튼 플로팅 버튼으로 변경

--- pages/users/profiles/[id].tsx
+++ pages/users/profiles/[id].tsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/router";
 import useUser from "@libs/client/useUser";
 import { CATEGORY, COLORS } from "@libs/string";
 import Button from "@components/button";
+import FloatingButton from "@components/floating-button";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -88,7 +89,7 @@ const Profile: NextPage<{ profile: ProfileWithProductAndReview }> = ({
   });
   const onReportClick = () => {
     router.push(`/report/${profile?.id}`);
-  }
+  };
 
   useEffect(() => {
     if (profile) {
@@ -191,13 +192,26 @@ const Profile: NextPage<{ profile: ProfileWithProductAndReview }> = ({
                 </div>
               </div>
             </div>
+          </div>
         </div>
-        </div>
-        <Button
-            onClick={onReportClick}
-            text="🚨"
-            disabled={me?.id === profile?.id}
-          />
+        {me?.id !== profile?.id && (
+          <FloatingButton href={`/report/${profile?.id}`}>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              ></path>
+            </svg>
+          </FloatingButton>
+        )}
         {profile?.products.length !== 0 && (
           <>
             <div className="flex flex-row border-b p-5">
