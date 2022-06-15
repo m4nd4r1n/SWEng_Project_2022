e290a8650d502afa5021467003d41921067452fd
m4nd4r1n <kara.mandarin99@gmail.com>
2022-06-15 17:01:20 +0900

    판매내역 판매중 리스트 추가

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import Item from "./item";
 
 interface ProductListProps {
-  kind: "favs" | "sales" | "purchases";
+  kind: "favs" | "sales" | "purchases" | "onsale";
 }
 
 interface Record {
@@ -18,22 +18,42 @@ interface ProductListResponse {
 export default function ProductList({ kind }: ProductListProps) {
   const { data } = useSWR<ProductListResponse>(`/api/users/me/${kind}`);
   return data ? (
-    <>
-      {data[kind]?.map((record) => (
-        <Item
-          id={record.product.id}
-          key={record.id}
-          title={record.product.name}
-          price={record.product.price}
-          categoryId={record.product.categoryId}
-          address={
-            record.product.address?.sido + " " + record.product.address?.sigungu
-          }
-          hearts={record.product._count.favs}
-          image={record.product.image}
-          onSale={record.product.onSale}
-        />
-      ))}
-    </>
+    kind !== "onsale" ? (
+      <>
+        {data[kind]?.map((record) => (
+          <Item
+            id={record.product.id}
+            key={record.id}
+            title={record.product.name}
+            price={record.product.price}
+            categoryId={record.product.categoryId}
+            address={
+              record.product.address?.sido +
+              " " +
+              record.product.address?.sigungu
+            }
+            hearts={record.product._count.favs}
+            image={record.product.image}
+            onSale={record.product.onSale}
+          />
+        ))}
+      </>
+    ) : (
+      <>
+        {data[kind]?.map((product: any) => (
+          <Item
+            id={product.id}
+            key={product.id}
+            title={product.name}
+            price={product.price}
+            categoryId={product.categoryId}
+            address={product.address?.sido + " " + product.address?.sigungu}
+            hearts={product._count.favs}
+            image={product.image}
+            onSale={product.onSale}
+          />
+        ))}
+      </>
+    )
   ) : null;
 }
--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,12 +1,39 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
 import ProductList from "@components/product-list";
+import { useState } from "react";
+import { cls } from "@libs/client/utils";
 
 const Sold: NextPage = () => {
+  const [state, setState] = useState<"onsale" | "sales">("onsale");
   return (
     <Layout title="판매내역" canGoBack seoTitle="Sale History">
+      <div className="grid h-10 w-full grid-cols-2 border-b">
+        <button
+          className={cls(
+            "border-b-2 pb-2 text-lg font-medium",
+            state === "onsale"
+              ? " border-orange-500 text-orange-400"
+              : "border-transparent text-gray-500 transition-colors hover:text-gray-400"
+          )}
+          onClick={() => (state !== "onsale" ? setState("onsale") : null)}
+        >
+          판매중
+        </button>
+        <button
+          className={cls(
+            "border-b-2 pb-2 text-lg font-medium",
+            state === "sales"
+              ? " border-orange-500 text-orange-400"
+              : "border-transparent text-gray-500 transition-colors hover:text-gray-400"
+          )}
+          onClick={() => (state !== "sales" ? setState("sales") : null)}
+        >
+          판매완료
+        </button>
+      </div>
       <div className="flex flex-col space-y-5 divide-y  pb-10">
-        <ProductList kind="sales" />
+        <ProductList kind={state} />
       </div>
     </Layout>
   );
